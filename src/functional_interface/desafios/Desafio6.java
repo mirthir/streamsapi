dc06cd211a16fb77ffac49fa26e97a0e88db0143
mirthir <mirthirbass@gmail.com>
2023-09-06 09:19:59 -0300

    desafios stream java

--- src/functional_interface/desafios/Desafio6.java
+++ src/functional_interface/desafios/Desafio6.java
@@ -5,7 +5,7 @@ import java.util.List;
 
 public class Desafio6 {
     public static void main(String[] args) {
-        List<Integer> numeros = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 15, 4, 3);
+        List<Integer> numeros = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 5, 4, 3);
         //verifique se  existe algum numero maior do que dez
         boolean existeMaiorQueDez = numeros.stream()
                 .anyMatch(numero -> numero > 10);
